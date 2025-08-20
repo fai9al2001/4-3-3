1f0676ebe02c86981a6baae45a1fdd9085169f29
fai9al2001 <fai9al2001@gmail.com>
2025-08-21 02:47:23 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+const database = {
+  "england": {
+    "name": "إنجلترا",
+    "leagues": {
+      "premier-league": {
+        "name": "Premier League",
+        "clubs": [
+          "Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Ipswich Town", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Southampton", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"
+        ]
+      }
+    }
+  },
+  "spain": {
+    "name": "إسبانيا",
+    "leagues": {
+      "laliga": {
+        "name": "La Liga",
+        "clubs": [
+          "Alavés", "Athletic Club", "Atlético Madrid", "Barcelona", "Celta Vigo", "Espanyol", "Getafe", "Girona", "Las Palmas", "Leganés", "Mallorca", "Osasuna", "Rayo Vallecano", "Real Betis", "Real Madrid", "Real Sociedad", "Sevilla", "Valencia", "Valladolid", "Villarreal"
+        ]
+      }
+    }
+  },
+  "germany": {
+    "name": "ألمانيا",
+    "leagues": {
+      "bundesliga": {
+        "name": "Bundesliga",
+        "clubs": [
+          "Augsburg", "Bayer Leverkusen", "Bayern Munich", "Bochum", "Borussia Dortmund", "Borussia Mönchengladbach", "Eintracht Frankfurt", "Freiburg", "Heidenheim", "Hoffenheim", "Holstein Kiel", "Mainz 05", "RB Leipzig", "St. Pauli", "Stuttgart", "Union Berlin", "Werder Bremen", "Wolfsburg"
+        ]
+      }
+    }
+  },
+  "italy": {
+    "name": "إيطاليا",
+    "leagues": {
+      "serie-a": {
+        "name": "Serie A",
+        "clubs": [
+          "Atalanta", "Bologna", "Cagliari", "Como", "Empoli", "Fiorentina", "Genoa", "Hellas Verona", "Inter Milan", "Juventus", "Lazio", "Lecce", "AC Milan", "Monza", "Napoli", "Parma", "Roma", "Torino", "Udinese", "Venezia"
+        ]
+      }
+    }
+  },
+  "france": {
+    "name": "فرنسا",
+    "leagues": {
+      "ligue-1": {
+        "name": "Ligue 1",
+        "clubs": [
+          "Angers", "Auxerre", "Brest", "Le Havre", "Lens", "Lille", "Lyon", "Marseille", "Monaco", "Montpellier", "Nantes", "Nice", "Paris Saint-Germain", "Reims", "Rennes", "Saint-Étienne", "Strasbourg", "Toulouse"
+        ]
+      }
+    }
+  }
+};
