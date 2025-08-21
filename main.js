df187c72132364f5d1a87484f8025013973be753
fai9al2001 <fai9al2001@gmail.com>
2025-08-21 08:07:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -127,7 +127,7 @@ clubFilenameOverrides['Spain - LaLiga'] = {
 
   // Map internal country IDs to ISO 3166-1 alpha-2 codes for flag-icons
   const countryIsoMap = {
-    england: 'gb', // Using GB flag for England overall (alternative: custom if desired)
+    england: 'gb-eng', // Use St George's Cross for England
     spain: 'es',
     germany: 'de',
     italy: 'it',
@@ -136,7 +136,7 @@ clubFilenameOverrides['Spain - LaLiga'] = {
     portugal: 'pt',
     belgium: 'be',
     turkey: 'tr',
-    scotland: 'gb-sct', // flag-icons supports subdivision codes like gb-sct
+    scotland: 'gb-sct', // Scotland saltire
     'saudi-arabia': 'sa'
   };
 
@@ -144,12 +144,12 @@ clubFilenameOverrides['Spain - LaLiga'] = {
   const flagsGrid = document.getElementById('flags-grid');
 
   // Full ISO country list (alpha-2) including FIFA-recognized + common territories, for flag-icons
-  const ISO_COUNTRY_CODES = `af ax al dz as ad ao ai aq ag ar am aw au at az bs bh bd bb by be bz bj bm bt bo bq ba bw bv br io bn bg bf bi kh cm ca cv ky cf td cl cn cx cc co km cg cd ck cr ci hr cu cw cy cz dk dj dm do ec eg sv gq er ee sz et fk fo fj fi fr gf pf tf ga gm ge de gh gi gr gl gd gp gu gt gg gn gw gy ht hm va hn hk hu is in id ir iq ie im il it jm jp je jo kz ke ki kp kr kw kg la lv lb ls lr ly li lt lu mo mk mg mw my mv ml mt mh mq mr mu yt mx fm md mc mn me ms ma mz mm na nr np nl nc nz ni ne ng nu nf mp no om pk pw ps pa pg py pe ph pn pl pt pr qa ro ru rw re bl sh kn lc mf pm vc ws sm st sa sn rs sc sl sg sx sk si sb so za gs ss es lk sd sr sj se ch sy tw tj tz th tl tg tk to tt tn tr tm tc tv ug ua ae gb us um uy uz vu ve vn vg vi wf eh ye zm zw`.split(/\s+/);
+  const ISO_COUNTRY_CODES = `af ax al dz as ad ao ai aq ag ar am aw au at az bs bh bd bb by be bz bj bm bt bo bq ba bw bv br io bn bg bf bi kh cm ca cv ky cf td cl cn cx cc co km cg cd ck cr ci hr cu cw cy cz dk dj dm do ec eg sv gq er ee sz et fk fo fj fi fr gf pf tf ga gm ge de gh gi gr gl gd gp gu gt gg gn gw gy ht hm va hn hk hu is in id ir iq ie im il it jm jp je jo kz ke ki kp kr kw kg la lv lb ls lr ly li lt lu mo mk mg mw my mv ml mt mh mq mr mu yt mx fm md mc mn me ms ma mz mm na nr np nl nc nz ni ne ng nu nf mp no om pk pw ps pa pg py pe ph pn pl pt pr qa ro ru rw re bl sh kn lc mf pm vc ws sm st sa sn rs sc sl sg sx sk si sb so za gs ss es lk sd sr sj se ch sy tw tj tz th tl tg tk to tt tn tr tm tc tv ug ua ae gb us um uy uz vu ve vn vg vi wf eh ye zm zw gb-eng gb-sct gb-wls`.split(/\s+/);
 
   // Optional Arabic names (fallback to code upper-case)
   const isoDisplayName = (code) => {
     const map = {
-      sa: 'السعودية', eg: 'مصر', dz: 'الجزائر', ma: 'المغرب', tn: 'تونس', qa: 'قطر', ae: 'الإمارات', kw: 'الكويت', bh: 'البحرين', om: 'عُمان', iq: 'العراق', sy: 'سوريا', jo: 'الأردن', lb: 'لبنان', ps: 'فلسطين', ye: 'اليمن', ly: 'ليبيا', sd: 'السودان', so: 'الصومال', gb: 'المملكة المتحدة', us: 'الولايات المتحدة', fr: 'فرنسا', de: 'ألمانيا', es: 'إسبانيا', it: 'إيطاليا', br: 'البرازيل', ar: 'الأرجنتين', pt: 'البرتغال', nl: 'هولندا', be: 'بلجيكا', tr: 'تركيا', jp: 'اليابان', cn: 'الصين', ru: 'روسيا', in: 'الهند', se: 'السويد', no: 'النرويج', fi: 'فنلندا', dk: 'الدانمارك', ch: 'سويسرا', at: 'النمسا', gr: 'اليونان', mx: 'المكسيك', ca: 'كندا', au: 'أستراليا', za: 'جنوب أفريقيا'
+      sa: 'السعودية', eg: 'مصر', dz: 'الجزائر', ma: 'المغرب', tn: 'تونس', qa: 'قطر', ae: 'الإمارات', kw: 'الكويت', bh: 'البحرين', om: 'عُمان', iq: 'العراق', sy: 'سوريا', jo: 'الأردن', lb: 'لبنان', ps: 'فلسطين', ye: 'اليمن', ly: 'ليبيا', sd: 'السودان', so: 'الصومال', gb: 'المملكة المتحدة', 'gb-eng': 'إنجلترا', 'gb-sct': 'اسكتلندا', 'gb-wls': 'ويلز', us: 'الولايات المتحدة', fr: 'فرنسا', de: 'ألمانيا', es: 'إسبانيا', it: 'إيطاليا', br: 'البرازيل', ar: 'الأرجنتين', pt: 'البرتغال', nl: 'هولندا', be: 'بلجيكا', tr: 'تركيا', jp: 'اليابان', cn: 'الصين', ru: 'روسيا', in: 'الهند', se: 'السويد', no: 'النرويج', fi: 'فنلندا', dk: 'الدانمارك', ch: 'سويسرا', at: 'النمسا', gr: 'اليونان', mx: 'المكسيك', ca: 'كندا', au: 'أستراليا', za: 'جنوب أفريقيا'
     };
     return map[code] || code.toUpperCase();
   };
